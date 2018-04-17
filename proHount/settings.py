771d33e2979f00b4a9b0743cfa1b8658f3190948
ubuntu <TobiasFaller@gmx.de>
2018-04-17 07:14:49 -0700

    second first commit

--- proHount/settings.py
+++ proHount/settings.py
@@ -75,8 +75,13 @@ WSGI_APPLICATION = 'proHount.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'prohuntdb',
+        'USER': 'postgres' ,
+        'PASSWORD': 'django1234' ,
+        'HOST': 'localhost',
+        'PORT': '5432',
+
     }
 }
 
